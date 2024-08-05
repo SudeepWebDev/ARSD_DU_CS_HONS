d378fdf38a4e44bc3af984fb4ef2e5fdbb9cebde
Sudeep Kumar Singh <127820890+SudeepWebDev@users.noreply.github.com>
2024-08-05 08:24:19 +0530

    Update

---
+++ rqs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+void quick_sort(vector<int>& arr, int left, int right) {
+    if (left >= right) return;
+
+    srand(time(nullptr));
+    int pivot_index = left + rand() % (right - left + 1);
+    int pivot = arr[pivot_index];
+    swap(arr[pivot_index], arr[right]);
+
+    int i = left;
+    for (int j = left; j < right; ++j) {
+        if (arr[j] < pivot) {
+            swap(arr[i], arr[j]);
+            ++i;
+        }
+    }
+    swap(arr[i], arr[right]);
+
+    quick_sort(arr, left, i - 1);
+    quick_sort(arr, i + 1, right);
+}
+
+int main() {
+    vector<int> arr = {3, 6, 8, 10, 1, 2, 1};
+    quick_sort(arr, 0, arr.size() - 1);
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
