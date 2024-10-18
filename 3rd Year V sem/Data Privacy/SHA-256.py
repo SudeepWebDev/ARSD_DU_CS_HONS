80616199ea30534c99b7e615ca86317721624190
Sudeep Kumar Singh <127820890+SudeepWebDev@users.noreply.github.com>
2024-10-18 09:50:39 +0530

    Update SHA-256.py

--- 3rd Year V sem/Data Privacy/SHA-256.py
+++ 3rd Year V sem/Data Privacy/SHA-256.py
@@ -1,1 +1,10 @@
+import hashlib
 
+def hash_password(password):
+    sha_signature = hashlib.sha256(password.encode()).hexdigest()
+    return sha_signature
+
+# Example usage
+password = input("Enter a password to hash: ")
+hashed_password = hash_password(password)
+print(f"SHA-256 hashed password: {hashed_password}")
